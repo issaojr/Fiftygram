8d3868b45a308a945c136ca95d3acb6b8531122c
IssaoJr <issaojr@gmail.com>
2020-08-20 00:48:44 -0300

    Invert Feature added and implemented; Save filtered photo implemented.

--- app/src/main/java/edu/harvard/cs50/fiftygram/MainActivity.java
+++ app/src/main/java/edu/harvard/cs50/fiftygram/MainActivity.java
@@ -1,14 +1,19 @@
 package edu.harvard.cs50.fiftygram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -22,17 +27,25 @@ import java.io.IOException;
 import jp.wasabeef.glide.transformations.gpu.SepiaFilterTransformation;
 import jp.wasabeef.glide.transformations.gpu.SketchFilterTransformation;
 import jp.wasabeef.glide.transformations.gpu.ToonFilterTransformation;
+import jp.wasabeef.glide.transformations.gpu.InvertFilterTransformation;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
     private ImageView imageView;
     private Bitmap original;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.image_view);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     public void apply(Transformation<Bitmap> filter) {
@@ -57,6 +70,8 @@ public class MainActivity extends AppCompatActivity {
         apply(new SketchFilterTransformation());
     }
 
+    public void applyInvert(View view) { apply(new InvertFilterTransformation()); }
+
     public void choosePhoto(View view) {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.setType("image/*");
@@ -69,15 +84,26 @@ public class MainActivity extends AppCompatActivity {
         if (resultCode == Activity.RESULT_OK && data != null) {
             try {
                 Uri uri = data.getData();
+                assert uri != null;
                 ParcelFileDescriptor parcelFileDescriptor =
                         getContentResolver().openFileDescriptor(uri, "r");
+                assert parcelFileDescriptor != null;
                 FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
                 original = BitmapFactory.decodeFileDescriptor(fileDescriptor);
                 parcelFileDescriptor.close();
+                imageView.setDrawingCacheEnabled(true);
                 imageView.setImageBitmap(original);
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    public void savePhoto(View view) {
+        if (original != null) {
+            Bitmap bmp = imageView.getDrawingCache();
+            Context context = view.getContext();
+            MediaStore.Images.Media.insertImage(context.getContentResolver(), bmp, "filtered", null);
         }
     }
 }
